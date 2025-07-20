9dfbc6f30ce24ab0ccd5571cd205f552ae06dcae
Miftah Ahmad Royhan Kurniawan <bhayuteguhsamudra@gmail.com>
2025-07-20 10:47:04 +0700

    Update ChemCalc.py

--- ChemCalc.py
+++ ChemCalc.py
@@ -502,20 +502,20 @@ def about_page():
 # APLIKASI UTAMA STREAMLIT (ROUTER)
 # =================================================================================
 
-image_link = "https://imgur.com/a/IcVQgDO.png"
 def set_custom_background(image_url):
     st.markdown(f"""
         <style>
         .stApp {{
             background: linear-gradient(rgba(0,0,0,0.75), rgba(0,0,0,0.75)), 
-                        url("{image_url}");
+                        url("{https://imgur.com/a/IcVQgDO.png}");
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
         }}
         </style>
         """, unsafe_allow_html=True)
-
+        
+image_link = "https://imgur.com/a/IcVQgDO.png"
 set_custom_background(image_link)
 
 def main():
