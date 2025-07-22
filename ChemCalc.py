5f942541b12418d9f77c909c3740010af435e215
Miftah Ahmad Royhan Kurniawan <bhayuteguhsamudra@gmail.com>
2025-07-22 16:58:59 +0700

    Update ChemCalc.py

--- ChemCalc.py
+++ ChemCalc.py
@@ -1,16 +1,5 @@
-
-st.markdown("""
-    <style>
-    .stApp {
-        background: linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.85)),
-                    url('https://github.com/Miproyy/ChemCalc.id/blob/main/Organic%20Chemistry%20Education%20Presentation%20in%20Blue%20Illustrative%20Style.png');
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-    }
-    </style>
-""", unsafe_allow_html=True)
+    import streamlit as st
+    import re
 
 # =================================================================================
 # DATA UNSUR KIMIA
