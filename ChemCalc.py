11c6d59700465a842227e501bde8f2c7202710dc
Miftah Ahmad Royhan Kurniawan <bhayuteguhsamudra@gmail.com>
2025-07-22 16:53:11 +0700

    Update ChemCalc.py

--- ChemCalc.py
+++ ChemCalc.py
@@ -562,7 +562,21 @@ def main():
         """,
         unsafe_allow_html=True
     )
+# PENYESUAIAN: CSS untuk tema gelap solid dan watermark
+    import streamlit as st
 
+st.markdown("""
+    <style>
+    .stApp {
+        background: linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.85)),
+                    url('https://github.com/Miproyy/ChemCalc.id/blob/main/Organic%20Chemistry%20Education%20Presentation%20in%20Blue%20Illustrative%20Style.png');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }
+    </style>
+""", unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
